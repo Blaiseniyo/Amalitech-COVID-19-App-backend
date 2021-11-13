3ac1765ccdcc63835332243f73b778d9e9d4fb35
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2021-11-13 15:32:12 +0200

    updation sender name on the email

--- src/helper/sendEmail.js
+++ src/helper/sendEmail.js
@@ -4,7 +4,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendEmail = async (userInfo) =>{
     const mailOptions = {
-      from:{name:"Awesomity",email:process.env.SENDGRID_SENDER_EMAIL},
+      from:{name:"Amalitech",email:process.env.SENDGRID_SENDER_EMAIL},
       to: userInfo.email,
       subject: userInfo.subject,
       html: userInfo.body
